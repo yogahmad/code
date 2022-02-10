a99539bbff07abcd60f9a198112f4ff7d0f73b0c
yogahmad <pratamay735@gmail.com>
2022-02-10 04:49:44 +0000

    Update generate team data

--- scripts/services/generate_team_data.py
+++ scripts/services/generate_team_data.py
@@ -17,16 +17,39 @@ class GenerateTeamDataService(Runnable):
         team_data = team_data_serializer.validated_data.get("teams")
 
         created_teams = []
+        updated_teams = []
         for team in team_data:
-            new_team = Team(
-                fpl_id=team.get("id"),
-                name=team.get("name"),
-                short_name=team.get("short_name"),
-                strength_attack_home=team.get("strength_attack_home"),
-                strength_defence_home=team.get("strength_defence_home"),
-                strength_attack_away=team.get("strength_attack_away"),
-                strength_defence_away=team.get("strength_defence_away"),
-            )
-            created_teams.append(new_team)
+            try:
+                existing_team = Team.objects.get(name=team.get("name"))
+                existing_team.fpl_id = team.get("id")
+                existing_team.short_name = team.get("short_name")
+                existing_team.strength_attack_home = team.get(
+                    "strength_attack_home")
+                existing_team.strength_attack_away = team.get(
+                    "strength_attack_away")
+                existing_team.strength_defence_home = team.get(
+                    "strength_defence_home")
+                existing_team.strength_defence_away = team.get(
+                    "strength_defence_away")
+                updated_teams.append(existing_team)
+            except Team.DoesNotExist:
+                new_team = Team(
+                    fpl_id=team.get("id"),
+                    name=team.get("name"),
+                    short_name=team.get("short_name"),
+                    strength_attack_home=team.get("strength_attack_home"),
+                    strength_attack_away=team.get("strength_attack_away"),
+                    strength_defence_home=team.get("strength_defence_home"),
+                    strength_defence_away=team.get("strength_defence_away"),
+                )
+                created_teams.append(new_team)
 
         Team.objects.bulk_create(created_teams)
+        Team.objects.bulk_update(existing_team, [
+            "fpl_id",
+            "short_name",
+            "strength_attack_home",
+            "strength_attack_away",
+            "strength_defence_home",
+            "strength_defence_away",
+        ])
